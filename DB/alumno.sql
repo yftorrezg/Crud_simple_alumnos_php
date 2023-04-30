b6543158bb28737aded92e408e3fe308f23d9380
Yafer Torrez <fer.torrez@outlook.es>
2023-04-30 19:55:27 -0400

    update db

--- DB/alumno.sql
+++ DB/alumno.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 01-05-2023 a las 01:51:35
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 8.1.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `alumno`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `alumno`
---
 
 CREATE TABLE `alumno` (
   `cod_estudiante` int(11) NOT NULL,
@@ -34,10 +9,6 @@ CREATE TABLE `alumno` (
   `apellidos` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `alumno`
---
-
 INSERT INTO `alumno` (`cod_estudiante`, `dni`, `nombres`, `apellidos`) VALUES
 (1, 123, 'Valeria', 'Flores'),
 (2, 123, 'Linda Arancha', 'jurado'),
@@ -45,27 +16,10 @@ INSERT INTO `alumno` (`cod_estudiante`, `dni`, `nombres`, `apellidos`) VALUES
 (4, 123, 'Tati', 'Damaris'),
 (5, 123, 'Iris', 'Chaparro');
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `alumno`
---
 ALTER TABLE `alumno`
   ADD PRIMARY KEY (`cod_estudiante`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `alumno`
---
 ALTER TABLE `alumno`
   MODIFY `cod_estudiante` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
